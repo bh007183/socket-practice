8371def1d8e061765e0a83602fd129d77bec519a
Ben Hopkins <bjhops17@gmail.com>
2021-06-25 16:21:24 -0700

    added basic boxing

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -3,7 +3,16 @@ import "./style.css"
 
 export default function Dashboard() {
     return (
-        <div>
+        <div id="DashContain">
+            <div id="bar">
+
+            </div>
+            <div id="roomContain">
+
+            </div>
+            <div id="messages">
+
+            </div>
             
         </div>
     )
